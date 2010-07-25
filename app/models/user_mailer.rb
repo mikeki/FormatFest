0c79d32a4bc79e705b26e5137b95fceb1617fa1a
daniefdz <danie.fdz@gmail.com>
2010-07-25 15:38:52 -0500

    borrar algunos archivos

--- app/models/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-
-  def registration_confirmation(user)
-    recipients  "mail@mail.com"
-    subject     "Thank you for Registering"
-    body        :user => user
-  end  
-
-end
