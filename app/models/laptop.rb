f70962038f8eb609aa3a781745caa12a09057ade
daniefdz <danie.fdz@gmail.com>
2010-07-25 14:12:51 -0500

    agrego so en laptop

--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -1,26 +1,33 @@
 class Laptop < ActiveRecord::Base
-validates_presence_of :marca, :modelo, :color, :paquete, :respaldo
+validates_presence_of :marca, :modelo, :color, :paquete
 belongs_to :user
 has_many :colaborators, :dependent => :destroy
 has_one :program, :dependent => :destroy
 
 MARCA = [
-["HP - Hewlett-Packard", "HP"],
-["Dell", "Dell"],
-["Toshiba", "Toshiba"],
-["IBM", "IBM"],
+
 ["Acer", "Acer"],
+["Apple", "Apple"],
 ["Asus", "Asus"],
+["Dell", "Dell"],
 ["Fujitsu", "Fujitsu"],
 ["Gateway", "Gateway"],
+["HP - Hewlett-Packard", "HP"],
+["IBM", "IBM"],
 ["Lenovo", "Lenovo"],
 ["Sony-VAIO","VAIO"],
+["Toshiba", "Toshiba"],
 ["Otra", "Otra"]
 ]
 
 PAQUETE = [
 ["Básico", "basico"],
 ["Personalizado", "personalizado"]
+]
+
+SO = [
+["Windows 7", "Windows 7"],
+["Windows XP", "Windows xp"]
 ]
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100717205128) do
+ActiveRecord::Schema.define(:version => 20100725184930) do
 
   create_table "colaborators", :force => true do |t|
     t.integer  "user_id"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(:version => 20100717205128) do
     t.text     "comentarios"
     t.string   "usuario"
     t.string   "contra"
-    t.integer  "estado",      :default => 0
+    t.integer  "estado",      :limit => 255, :default => 0
     t.string   "paquete"
     t.boolean  "respaldo"
     t.integer  "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20100717205128) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "terms"
+    t.string   "so"
   end
 
   create_table "posts", :force => true do |t|
