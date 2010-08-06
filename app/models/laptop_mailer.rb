fc0a30540cc49a76341b8294b413d91ab6fad620
Miguel Cervera <miguel@miguel-laptop.(none)>
2010-08-06 17:39:08 -0500

    registrar boton

--- app/models/laptop_mailer.rb
+++ app/models/laptop_mailer.rb
@@ -2,14 +2,14 @@ class LaptopMailer < ActionMailer::Base
   
   def received_message(laptop, email)
     recipients email
-    from "FormatFest 17 <formatfest@saitc-mty.com>"
+    from "FormatFest 17 <contacto@formatfest.com>"
     subject "Confirmacion de recepcion de laptop #{laptop.marca}"
     body :laptop => laptop
   end  
   
   def end_message(laptop, email)
     recipients email
-    from "FormatFest 17 <formatfest@saitc-mty.com>"
+    from "FormatFest 17 <contacto@formatfest.com>"
     subject "Reparacion de laptop #{laptop.marca} finalizada"
     body :laptop => laptop
   end
--- config/environment.rb
+++ config/environment.rb
@@ -23,8 +23,8 @@ Rails::Initializer.run do |config|
 	config.gem 'will_paginate', :version => '2.3.14'
 	
   #HABILITAR AL HACER DEPLOYMENT
-  config.gem 'memcached-northscale', :lib => 'memcached'
-  require 'memcached'
+  #config.gem 'memcached-northscale', :lib => 'memcached'
+  #require 'memcached'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -53,7 +53,7 @@ end
 require 'tlsmail'                                                                  #
                                                                                    #
 # Es el email a donde se enviaran los correos                                      #
-CONTACT_RECIPIENT = 'formatfest@saitc-mty.com'                                     #
+CONTACT_RECIPIENT = 'contacto@formatfest.com'                                      #
                                                                                    #
 # Gmail utiliza TLS, por lo que se habilita primero                                #
 Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)                                    #
@@ -67,12 +67,12 @@ ActionMailer::Base.default_charset = "utf-8"                                    
 ActionMailer::Base.raise_delivery_errors = true                                    #
 # Se configuran las propiedades de entrega de SMTP                                 #
 ActionMailer::Base.smtp_settings = {                                               #
-  :domain          => "formatfest@saitc-mty.com",                                  #
+  :domain          => "contacto@formatfest.com",                                   #
   :address         => 'smtp.gmail.com',                                            #
   :port            => 587,                                                         #
   :tls             => true,                                                        #
   :authentication  => :plain,                                                      #
-  :user_name       => 'formatfest@saitc-mty.com',                                  #
-  :password        => 'secreto'                                                    #
+  :user_name       => 'contacto@formatfest.com',                                   #
+  :password        => 'qwertyformat'                                               #
 }                                                                                  #
 ####################################################################################
