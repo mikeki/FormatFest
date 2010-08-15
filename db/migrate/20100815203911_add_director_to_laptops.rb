d9dd2b3f341fdb8b7049baf0fae49b3f546a4344
Miguel Cervera <miguel@miguel-laptop.(none)>
2010-08-15 16:21:54 -0500

    default

--- db/migrate/20100815203911_add_director_to_laptops.rb
+++ db/migrate/20100815203911_add_director_to_laptops.rb
@@ -1,6 +1,6 @@
 class AddDirectorToLaptops < ActiveRecord::Migration
   def self.up
-    add_column :laptops, :director, :boolean
+    add_column :laptops, :director, :boolean, :default => "0"
   end
 
   def self.down
