55c47b943386cafe470ffc765348e6d1e3d2a3e7
Miguel Cervera <miguel@miguel-laptop.(none)>
2011-01-07 14:32:47 -0600

    cambiso al mail y asi

--- app/models/laptop_mailer.rb
+++ app/models/laptop_mailer.rb
@@ -2,14 +2,14 @@ class LaptopMailer < ActionMailer::Base
   
   def registered_message(laptop, email)
     recipients email
-    from "FormatFest 17 <contacto@formatfest.com>"
+    from "FormatFest 18 <contacto@formatfest.com>"
     subject "Confirmacion de registro de laptop #{laptop.marca}"
     body :laptop => laptop
   end
   
   def end_message(laptop, email)
     recipients email
-    from "FormatFest 17 <contacto@formatfest.com>"
+    from "FormatFest 18 <contacto@formatfest.com>"
     subject "Reparacion de laptop #{laptop.marca} finalizada"
     body :laptop => laptop
   end
